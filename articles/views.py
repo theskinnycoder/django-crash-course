c0fa0c8638d7c744b30698e498b6a5733ee3339a
theskinnycoder <18131a0505@gvpce.ac.in>
2021-09-09 00:13:58 +0530

    Added order by

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,7 @@ from .models import Article
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.objects.all().order_by('-created_at')
     return render(request, 'articles/article_list.html', {'articles': articles})
 
 
