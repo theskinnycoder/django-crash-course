9128f00a4a291a0204217cf6c489927cae98bbd2
theskinnycoder <18131a0505@gvpce.ac.in>
2021-11-09 16:48:49 +0530

    No changes

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
-                                  TemplateView, UpdateView)
+from django.views.generic import (CreateView,
+                                  DeleteView,
+                                  DetailView,
+                                  ListView,
+                                  TemplateView,
+                                  UpdateView
+                                  )
 
 from .models import Article
 
