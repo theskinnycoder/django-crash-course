9237e9f1b0ef31fe6a5483c16849e5ffea88764f
theskinnycoder <18131a0505@gvpce.ac.in>
2021-11-09 17:06:29 +0530

    changed imports

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.views.generic.list import ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Article
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
 
